c1ac6b50ac746314f9bcb6377188d78b5badc082
Mikio Hara <mikioh.mikioh@gmail.com>
2017-08-23 05:31:34 +0900

    ipoam: drop support for Go 1.6 or below

--- benchmark_test.go
+++ benchmark_test.go
@@ -12,43 +12,35 @@ import (
 	"github.com/mikioh/ipoam"
 )
 
-func BenchmarkTesterGoogleIPv4(b *testing.B) {
-	ipt, err := ipoam.NewTester("ip4:icmp", "0.0.0.0")
-	if err != nil {
-		b.Log(err)
-		return
-	}
-	defer ipt.Close()
-	ips, err := net.LookupIP("ipv4.google.com")
-	if err != nil {
-		b.Log(err)
-		return
-	}
-	benchmarkLoopbackGoogle(b, ipt, ips[0])
-}
-
-func BenchmarkTesterGoogleIPv6(b *testing.B) {
-	ipt, err := ipoam.NewTester("ip6:ipv6-icmp", "::")
-	if err != nil {
-		b.Log(err)
-		return
-	}
-	defer ipt.Close()
-	ips, err := net.LookupIP("ipv6.google.com")
-	if err != nil {
-		b.Log(err)
-		return
-	}
-	benchmarkLoopbackGoogle(b, ipt, ips[0])
-}
-
-func benchmarkLoopbackGoogle(b *testing.B, ipt *ipoam.Tester, ip net.IP) {
-	cm := ipoam.ControlMessage{ID: os.Getpid() & 0xffff}
-	for i := 0; i < b.N; i++ {
-		cm.Seq = i + 1
-		if err := ipt.Probe([]byte("HELLO-R-U-THERE"), &cm, ip, nil); err != nil {
-			b.Fatal(err)
-		}
-		<-ipt.Report()
+func BenchmarkTester(b *testing.B) {
+	for _, bb := range []struct {
+		name                     string
+		network, address, target string
+	}{
+		{"IPv4", "ip4:icmp", "0.0.0.0", "ipv4.google.com"},
+		{"IPv6", "ip6:ipv6-icmp", "::", "ipv6.google.com"},
+	} {
+		b.Run(bb.name, func(b *testing.B) {
+			ipt, err := ipoam.NewTester(bb.network, bb.address)
+			if err != nil {
+				b.Log(err)
+				return
+			}
+			defer ipt.Close()
+			ips, err := net.LookupIP(bb.target)
+			if err != nil {
+				b.Log(err)
+				return
+			}
+			b.ResetTimer()
+			cm := ipoam.ControlMessage{ID: os.Getpid() & 0xffff}
+			for i := 0; i < b.N; i++ {
+				cm.Seq = i + 1
+				if err := ipt.Probe([]byte("HELLO-R-U-THERE"), &cm, ips[0], nil); err != nil {
+					b.Fatal(err)
+				}
+				<-ipt.Report()
+			}
+		})
 	}
 }
--- tester_test.go
+++ tester_test.go
@@ -13,148 +13,114 @@ import (
 	"github.com/mikioh/ipoam"
 )
 
-var testerGlobalUnicastTests = []struct {
-	network, address, dst string
-}{
-	{"ip4:icmp", "0.0.0.0", "golang.org"},
-	{"udp", "0.0.0.0:0", "golang.org"},
-
-	{"ip6:ipv6-icmp", "::", "golang.org"},
-	{"udp", "[::]:0", "golang.org"},
-
-	{"ip4:icmp", "0.0.0.0", "www.google.com"},
-	{"udp", "0.0.0.0:0", "www.google.com"},
-
-	{"ip6:ipv6-icmp", "::", "www.google.com"},
-	{"udp", "[::]:0", "www.google.com"},
-}
-
 func TestTesterGlobalUnicast(t *testing.T) {
 	if testing.Short() {
 		t.Skip("to avoid external network")
 	}
 
-	for i, tt := range testerGlobalUnicastTests {
-		cm := ipoam.ControlMessage{ID: os.Getpid()&0xffff + i, Port: 33434}
-		ipt, err := ipoam.NewTester(tt.network, tt.address)
-		if err != nil {
-			t.Log(err)
-			continue
-		}
-		defer ipt.Close()
+	for i, tt := range []struct {
+		name                     string
+		network, address, target string
+		ip                       net.IP
+	}{
+		{"GlobalUnicast", "ip4:icmp", "0.0.0.0", "golang.org", nil},
+		{"GlobalUnicast", "udp", "0.0.0.0:0", "golang.org", nil},
 
-		ips, err := net.LookupIP(tt.dst)
-		if err != nil {
-			t.Error(err)
-			continue
-		}
-		nips := ips[:0]
-		for _, ip := range ips {
-			if ip.To4() != nil && ipt.IPv4PacketConn() != nil || ip.To16() != nil && ip.To4() == nil && ipt.IPv6PacketConn() != nil {
-				nips = append(nips, ip)
-			}
-		}
-		ips = nips
+		{"GlobalUnicast", "ip6:ipv6-icmp", "::", "golang.org", nil},
+		{"GlobalUnicast", "udp", "[::]:0", "golang.org", nil},
 
-		cm.Seq = i + 1
-		for _, ip := range ips {
-			if err := ipt.Probe([]byte("HELLO-R-U-THERE"), &cm, ip, nil); err != nil {
+		{"GlobalUnicast", "ip4:icmp", "0.0.0.0", "www.google.com", nil},
+		{"GlobalUnicast", "udp", "0.0.0.0:0", "www.google.com", nil},
+
+		{"GlobalUnicast", "ip6:ipv6-icmp", "::", "www.google.com", nil},
+		{"GlobalUnicast", "udp", "[::]:0", "www.google.com", nil},
+
+		{"LinkLocalMulticast", "ip4:icmp", "0.0.0.0", "", net.IPv4(224, 0, 0, 251)},
+
+		{"LinkLocalMulticast", "ip6:ipv6-icmp", "::", "", net.ParseIP("ff02::1")},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			cm := ipoam.ControlMessage{ID: os.Getpid()&0xffff + i, Port: 33434}
+			ipt, err := ipoam.NewTester(tt.network, tt.address)
+			if err != nil {
 				t.Log(err)
+				return
 			}
-		}
+			defer ipt.Close()
 
-		wait := time.NewTimer(250 * time.Millisecond)
-		var rs []ipoam.Report
-	loop:
-		for {
-			select {
-			case <-wait.C:
-				break loop
-			case r := <-ipt.Report():
-				rs = append(rs, r)
+			var ips []net.IP
+			var mift []net.Interface
+			if tt.target != "" {
+				ips, err = net.LookupIP(tt.target)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+				nips := ips[:0]
+				for _, ip := range ips {
+					if ip.To4() != nil && ipt.IPv4PacketConn() != nil || ip.To16() != nil && ip.To4() == nil && ipt.IPv6PacketConn() != nil {
+						nips = append(nips, ip)
+					}
+				}
+				ips = nips
+			} else {
+				ift, err := net.Interfaces()
+				if err != nil {
+					t.Error(err)
+					return
+				}
+				mift = ift[:0]
+				for _, ifi := range ift {
+					if ifi.Flags&net.FlagUp == 0 || ifi.Flags&net.FlagMulticast == 0 {
+						continue
+					}
+					mift = append(mift, ifi)
+				}
 			}
-		}
-		wait.Stop()
 
-		if len(rs) == 0 {
-			t.Logf("got no records for %s on %s, %s", tt.dst, tt.network, tt.address)
-		}
-		for _, r := range rs {
-			if r.Error != nil {
-				t.Logf("%s: %v", tt.dst, r.Error)
+			cm.Seq = i + 1
+			if tt.target != "" {
+				for _, ip := range ips {
+					if err := ipt.Probe([]byte("HELLO-R-U-THERE"), &cm, ip, nil); err != nil {
+						t.Log(err)
+					}
+				}
+			} else {
+				for j := range mift {
+					if err := ipt.Probe([]byte("HELLO-R-U-THERE"), &cm, tt.ip, &mift[j]); err != nil {
+						t.Log(err)
+					}
+				}
 			}
-			if r.ICMP != nil {
-				t.Logf("%s: %+v", tt.dst, r.ICMP)
+
+			wait := time.NewTimer(250 * time.Millisecond)
+			var rs []ipoam.Report
+		loop:
+			for {
+				select {
+				case <-wait.C:
+					break loop
+				case r := <-ipt.Report():
+					rs = append(rs, r)
+				}
 			}
-		}
-	}
-}
+			wait.Stop()
 
-var testerLinkLocalMulticastTests = []struct {
-	network, address string
-	ip               net.IP
-}{
-	{"ip4:icmp", "0.0.0.0", net.IPv4(224, 0, 0, 251)},
-
-	{"ip6:ipv6-icmp", "::", net.ParseIP("ff02::1")},
-}
-
-func TestTesterLinkLocalMulticast(t *testing.T) {
-	if testing.Short() {
-		t.Skip("to avoid external network")
-	}
-
-	ift, err := net.Interfaces()
-	if err != nil {
-		t.Fatal(err)
-	}
-	mift := ift[:0]
-	for _, ifi := range ift {
-		if ifi.Flags&net.FlagUp == 0 || ifi.Flags&net.FlagMulticast == 0 {
-			continue
-		}
-		mift = append(mift, ifi)
-	}
-
-	for i, tt := range testerLinkLocalMulticastTests {
-		cm := ipoam.ControlMessage{ID: os.Getpid()&0xffff + i}
-		ipt, err := ipoam.NewTester(tt.network, tt.address)
-		if err != nil {
-			t.Log(err)
-			continue
-		}
-		defer ipt.Close()
-
-		cm.Seq = i + 1
-		for j := range mift {
-			if err := ipt.Probe([]byte("HELLO-R-U-THERE"), &cm, tt.ip, &mift[j]); err != nil {
-				t.Log(err)
+			tgt := tt.target
+			if tgt == "" {
+				tgt = tt.ip.String()
 			}
-		}
-
-		wait := time.NewTimer(250 * time.Millisecond)
-		var rs []ipoam.Report
-	loop:
-		for {
-			select {
-			case <-wait.C:
-				break loop
-			case r := <-ipt.Report():
-				rs = append(rs, r)
+			if len(rs) == 0 {
+				t.Logf("got no records for %s on %s, %s", tgt, tt.network, tt.address)
 			}
-		}
-		wait.Stop()
-
-		if len(rs) == 0 {
-			t.Logf("got no records for %v on %s, %s", tt.ip, tt.network, tt.address)
-		}
-		for _, r := range rs {
-			if r.Error != nil {
-				t.Logf("%v: %v %v", tt.ip, r.Error, r.OrigHeader)
+			for _, r := range rs {
+				if r.Error != nil {
+					t.Logf("%s: %v", tgt, r.Error)
+				}
+				if r.ICMP != nil {
+					t.Logf("%s: %+v", tgt, r.ICMP)
+				}
 			}
-			if r.ICMP != nil {
-				t.Logf("%v: %+v %v", tt.ip, r.ICMP, r.OrigHeader)
-			}
-		}
+		})
 	}
 }
